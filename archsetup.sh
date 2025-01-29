445e89b181a6ec24655c453b4c3583d0d109e3c0
SmooFarmFarm <90259401+SmooFarmFarm@users.noreply.github.com>
2025-01-28 22:42:01 -0500

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -1,14 +1,46 @@
 f#!/bin/bash
 set -e  # Exit on error
 
+#####################
+
+# Update system clock
+sudo timedatectl set-ntp true
+
+####################
+
+# Install reflector to find the fastest mirrors
+sudo pacman -S --noconfirm reflector
+
+# Backup existing mirrorlist
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+# Set the 10 fastest mirrors
+sudo reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Refresh package database
+sudo pacman -Syy
+
+###################
+
+
+# Enable TRIM for SSDs - not needed in virtualbox
+# sudo systemctl enable fstrim.timer
+
+
+##################
+
 echo "Updating system..."
 sudo pacman -Syu --noconfirm
+
+###################
+
 
 
 # Installing core packages
 echo "Installing essential packages..."
 sudo pacman -S --needed --noconfirm git nano base-devel flatpak gparted htop inxi obs-studio syncthing
 
+####################
 
 # Installing yay
 echo "Installing yay (AUR helper)..."
@@ -22,17 +54,24 @@ else
     echo "yay is already installed."
 fi
 
+###################
+
+
 # Enabling Flathub and downloading Flatpak apps
 echo "Enabling Flathub..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install com.spotify.Client org.standardnotes.standardnotes org.audacityteam.Audacity org.mozilla.firefox org.chromium.Chromium com.bitwarden.desktop com.calibre_ebook.calibre com.discordapp.Discord io.mpv.Mpv org.videolan.VLC org.qbittorrent.qBittorrent org.kde.kdenlive org.libreoffice.LibreOffice org.kde.krita org.signal.Signal            
 
+##################
+
 # AUR programs
 yay -S --noconfirm veracrypt mullvad-vpn
 
+
+#################
+
 # Set up 'll' alias like Ubuntu
 echo 'alias ll="ls -lah --color=auto"' | sudo tee -a /etc/skel/.bashrc /root/.bashrc /home/$USER/.bashrc
-
 
 # Reload bash config
 source /home/$USER/.bashrc
